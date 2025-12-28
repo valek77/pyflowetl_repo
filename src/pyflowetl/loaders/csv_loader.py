e71641f762833f677d49016c7291be89d917b6c5
Marco Zamparelli <marco.zamparelli@libero.it>
2025-12-28 11:54:56 +0100

    csv header

--- src/pyflowetl/loaders/csv_loader.py
+++ src/pyflowetl/loaders/csv_loader.py
@@ -1,10 +1,13 @@
 import os
+from email import header
+
 from pyflowetl.log import get_logger, log_memory_usage
 
 
 
 class CsvLoader:
-    def __init__(self, output_path, encoding="utf-8", delimiter=","):
+    def __init__(self, output_path, encoding="utf-8", delimiter=",", header=True):
+        self.header = header
         self.output_path = output_path
         self.encoding = encoding
         self.delimiter = delimiter
@@ -15,7 +18,7 @@ class CsvLoader:
 
         try:
             os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
-            data.to_csv(self.output_path, index=False, encoding=self.encoding, sep=self.delimiter)
+            data.to_csv(self.output_path, index=False, encoding=self.encoding, sep=self.delimiter, header=self.header)
             logger.info(f"[CsvLoader] Scrittura completata: {len(data)} record")
             log_memory_usage(f"Dopo Scrittura su file: {self.output_path}")
         except Exception as e:
